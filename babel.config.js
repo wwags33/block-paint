f60089dd9db4c6adc999910f02cf562a8328c6d1
wwags33 <wwags33@gmail.com>
2018-09-08 20:39:55 -0500

    Adding project config files.

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+const presets = [
+  ["@babel/env", {
+    targets: {
+      "browsers": ["last 3 versions", "> 5% in US"]
+    },
+    useBuiltIns: "usage"
+  }]
+];
+
+module.exports = { presets };
